70b37892ea886eaf658adcb98f2177c75b944b3f
Kha <khaho@teqnological.asia>
2017-10-29 17:59:04 +0700

    add navigation bar

--- Game4Kids/Base/BaseViewController.swift
+++ Game4Kids/Base/BaseViewController.swift
@@ -18,7 +18,6 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        playSound(name: "AllFallDown", Extension: "mp3")
     }
 }
 
--- Game4Kids/ViewController.swift
+++ Game4Kids/ViewController.swift
@@ -8,20 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
+
+    @IBOutlet weak var muteBtn: UIButton!
+    var isMute = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        playSound(name: "AllFallDown", Extension: "mp3")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func muteBtnDidTap(_ sender: Any) {
+        if isMute {
+            player?.play()
+            isMute = false
+            muteBtn.setImage(UIImage(named: "notMute"), for: .normal)
+        } else {
+            player?.pause()
+            isMute = true
+            muteBtn.setImage(UIImage(named: "mute"), for: .normal)
+        }
 
-    @IBAction func didTapBtn(_ sender: Any) {
-        self.navigationController?.pushViewController(GameViewController(), animated: true)
     }
 
 }
