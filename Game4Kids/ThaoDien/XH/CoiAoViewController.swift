f7f9d51d8e7639bf57d569bbf3434ee7c371132e
Kha <khaho@teqnological.asia>
2018-01-16 21:24:12 +0700

    add game

---
+++ Game4Kids/ThaoDien/XH/CoiAoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CoiAoViewController.swift
+//  Game4Kids
+//
+//  Created by KHA on 1/16/18.
+//  Copyright © 2018 Kha. All rights reserved.
+//
+
+import UIKit
+
+class CoiAoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
