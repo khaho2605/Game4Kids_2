cd669e27803858c0d0d6c22c469ec5403137c3dd
Kha <khaho@teqnological.asia>
2018-01-25 20:46:51 +0700

    fix bug

--- Game4Kids/Main/MainViewController.swift
+++ Game4Kids/Main/MainViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class MainViewController: BaseViewController {
-    static let sharedInstance = MainViewController()
-
     @IBOutlet weak var muteBtn: UIButton!
-    var isMute = false
+    static var tempMute = true
+     var isMute = true {
+        didSet {
+            MainViewController.tempMute = isMute
+            if isMute {
+                player?.play()
+                muteBtn.setImage(UIImage(named: "notMute"), for: .normal)
+            } else {
+                player?.pause()
+                muteBtn.setImage(UIImage(named: "mute"), for: .normal)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +32,7 @@ class MainViewController: BaseViewController {
     }
 
     @IBAction func muteBtnDidTap(_ sender: Any) {
-        if isMute {
-            player?.play()
-            isMute = false
-            muteBtn.setImage(UIImage(named: "notMute"), for: .normal)
-        } else {
-            player?.pause()
-            isMute = true
-            muteBtn.setImage(UIImage(named: "mute"), for: .normal)
-        }
+        isMute = !isMute
     }
 
     @objc func stopMusic() {
